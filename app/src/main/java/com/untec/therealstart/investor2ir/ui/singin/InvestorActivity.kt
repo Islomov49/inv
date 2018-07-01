c42f55c6b70845fc281c21370ce224690bd5b9f5
islomov49 <islomov49@gmail.com>
2018-07-01 23:20:17 +0500

    Push with Adding gradle configs and with package struct

--- app/src/main/java/com/untec/therealstart/investor2ir/ui/singin/InvestorActivity.kt
+++ app/src/main/java/com/untec/therealstart/investor2ir/ui/singin/InvestorActivity.kt
@@ -1,12 +1,14 @@
-package com.untec.therealstart.investor2ir
+package com.untec.therealstart.investor2ir.ui.singin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.untec.therealstart.investor2ir.R
 
 class InvestorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_investor)
+
     }
 }
---
+++ app/src/main/java/com/untec/therealstart/investor2ir/utils/AppGlideModule.kt
@@ -0,0 +1,7 @@
+package com.untec.therealstart.investor2ir.utils
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class AppGlideModule : AppGlideModule()
